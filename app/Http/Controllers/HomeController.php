9c377cfb7fe1db78c32a5456776dc540852b929e
hanhafizh <farhanhafizh770@gmail.com>
2025-01-12 11:02:57 +0700

    menampilkan data dari database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $data = User::get();
+        return view('index', compact('data'));
     }
 }
