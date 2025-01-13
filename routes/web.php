6131eaed5b1b788686c753059f8df327bbfa52c2
hanhafizh <farhanhafizh770@gmail.com>
2025-01-13 23:24:41 +0700

    login proses - sweet alert

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'dashboard']);
-Route::get('/user', [HomeController::class, 'index'])->name('index');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login_proses', [LoginController::class, 'login_proses'])->name('login.proses');
+
+Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+
+Route::get('/user', [HomeController::class, 'index'])->name('user.index');
 Route::get('/create', [HomeController::class, 'create'])->name('user.create');
 Route::post('/store', [HomeController::class, 'store'])->name('user.store');
 
